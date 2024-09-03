f6cab5c6d219617bbc5199168244c743e0134f42
autodataming <1017044504@qq.com>
2024-09-03 12:35:16 +0800

    Updated molstat: fixed plt.show() and hide warnings and handled SDF molecule errors

--- molstat.py
+++ molstat.py
@@ -10,6 +10,19 @@ import os
 import argparse
 
 
+
+import warnings
+
+# 忽略所有运行时警告
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+from rdkit import RDLogger
+
+# 关闭RDKit的警告信息
+lg = RDLogger.logger()
+lg.setLevel(RDLogger.CRITICAL)  # 设置日志级别为CRITICAL，忽略警告和错误信息
+
+
+
 def getDescriptors(mols_gen):
     desc ={}
 
@@ -19,17 +32,32 @@ def getDescriptors(mols_gen):
     hbds = []
     hbas = []
 
+    errid=0
+    totalid=0
     for mol in mols_gen:   
-        tpsa_m = Descriptors.TPSA(mol)
-        logp_m = Descriptors.MolLogP(mol)
-        mw_m = Descriptors.MolWt(mol)
-        hbd_m = Descriptors.NumHDonors(mol)
-        hba_m = Descriptors.NumHAcceptors(mol)
-        tpsas.append(tpsa_m)
-        logps.append(logp_m)
-        mws.append(mw_m)
-        hbds.append(hbd_m)
-        hbas.append(hba_m)
+        totalid+=1
+        try:
+            # 尝试清理分子，捕获清理过程中的错误
+            Chem.SanitizeMol(mol)
+            tpsa_m = Descriptors.TPSA(mol)
+            logp_m = Descriptors.MolLogP(mol)
+            mw_m = Descriptors.MolWt(mol)
+            hbd_m = Descriptors.NumHDonors(mol)
+            hba_m = Descriptors.NumHAcceptors(mol)
+            tpsas.append(tpsa_m)
+            logps.append(logp_m)
+            mws.append(mw_m)
+            hbds.append(hbd_m)
+            hbas.append(hba_m)
+        except:
+            errid+=1
+            smi = Chem.MolToSmiles(mol) if mol else "Invalid Molecule"
+            print("error smi:",errid,totalid,smi)
+
+
+
+
+
     desc['tpsa']=tpsas
     desc['logp']=logps 
     desc['mw']=mws 
@@ -48,7 +76,7 @@ def mwfigure(mws,outf='mw_stat.png',title="Molecular Weight Distribution",xlabel
     plt.ylabel(ylabel)
     plt.grid(False)
     plt.savefig(outf, dpi=300)  # Save the figure with 300 dpi resolution
-    plt.show()
+    # plt.show()
 
 
 def molstat(molf):
@@ -64,7 +92,7 @@ def molstat(molf):
     # Split the file name and extension
     basename, file_extension = os.path.splitext(file_name_with_ext)
     if file_extension=='.sdf':
-        mols_gen = Chem.SDMolSupplier(filename)
+        mols_gen = Chem.SDMolSupplier(filename, sanitize=False)
     else:
         print("%s extension format is not supported"%file_extension)
     
@@ -87,4 +115,5 @@ if __name__ == '__main__':
     '''
     '''
     filename = "test.sdf"
+    filename = "L3400.sdf"
     molstat(filename)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='molstat',
-    version='0.1',
+    version='0.2',
     py_modules=['molstat'],
     install_requires=[
         'numpy',
